cecd0c6e288b62c39f2bc0dd57b39904f0943e81
David Mehren <git@herrmehren.de>
2022-02-05 16:21:13 +0100

    fix(api/private/me): require and document displayName
    This renames the argument in the POST /profile route to `displayName`
    to be more consistent with the UserDTO.
    
    It also adds OpenAPI docs.
    
    Signed-off-by: David Mehren <git@herrmehren.de>

--- test/private-api/me.e2e-spec.ts
+++ test/private-api/me.e2e-spec.ts
@@ -110,7 +110,7 @@ describe('Me', () => {
     await agent
       .post('/api/private/me/profile')
       .send({
-        name: newDisplayName,
+        displayName: newDisplayName,
       })
       .expect(201);
     const dbUser = await testSetup.userService.getUserByUsername('hardcoded');
